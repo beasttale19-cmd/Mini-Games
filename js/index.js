bcd5856ce65448e65f6ad9f82aa22e8bfaa34a27
Nirjal Acharya <beasttale19@gmail.com>
2026-06-17 08:34:37 +0545

    There was a problem in background image loading by js in github. it was working on local files but not on github-url so i fixed it.

--- js/index.js
+++ js/index.js
@@ -4,14 +4,14 @@ let darkMode = true;
 document.getElementById("toggleClick").onclick = function () {
 
     if (darkMode) {
-        bodyStyle.style.backgroundImage = "url('../img/bg-forWhite.png')";
+        bodyStyle.style.backgroundImage = "url('img/bg-forWhite.png')";
         bodyStyle.style.backgroundColor = "white";
         bodyStyle.style.color = "black";
         darkMode = false;
     }
 
     else {
-        bodyStyle.style.backgroundImage = "url('../img/bg2.png')";
+        bodyStyle.style.backgroundImage = "url('img/bg2.png')";
         bodyStyle.style.backgroundColor = "black";
         bodyStyle.style.color = "white";
         darkMode = true;
