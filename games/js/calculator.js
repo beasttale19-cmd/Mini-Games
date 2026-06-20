5c39220f00e2c507316a40582a4b95e5cb893302
Nirjal Acharya <beasttale19@gmail.com>
2026-06-20 09:43:17 +0545

    I think i completed the calculator pretty much, eval() makes it very easy to calculate the answer. I also made error handling so if any error comes "Error" shows up. Everything works fine. Now, I can make changes in this after but for this time i will make something different.

--- games/js/calculator.js
+++ games/js/calculator.js
@@ -6,8 +6,31 @@ function press(value) {
         display.textContent = value;
     }
 
+    else if (display.textContent === "Error") {
+        display.textContent = value;
+        function calculate() {
+            display.textContent = 0;
+        }
+    }
+
     else {
         display.textContent += value;
     }
 }
 
+function clearAll() {
+    display.textContent = "0";
+}
+
+function calculate() {
+
+    try {
+        display.textContent = eval(display.textContent)
+    }
+
+    catch (error) {
+        display.textContent = "Error"
+    }
+
+
+}
