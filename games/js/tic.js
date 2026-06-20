c4f509caafdd007d5563204dc40ba15f3e8f6210
Nirjal Acharya <beasttale19@gmail.com>
2026-06-20 12:08:49 +0545

    Some adds in tic tac toe: added if press for the first time it presses X, in second time it presses O and third press X and like that.... Its taking too much time for me because i am trying one thing then one thing, cause im just learning and things are limited ig

--- games/js/tic.js
+++ games/js/tic.js
@@ -1,2 +1,31 @@
-let turn = "X";
+let turn = "O";
 
+let t1=document.getElementById("1");
+let t2=document.getElementById("2");
+let t3=document.getElementById("3");
+let t4=document.getElementById("4");
+let t5=document.getElementById("5");
+let t6=document.getElementById("6");
+let t7=document.getElementById("7");
+let t8=document.getElementById("8");
+let t9=document.getElementById("9");
+
+function press(value) {
+    const click = document.getElementById(value);
+    click.textContent = `${turn}`;
+
+    if (click.textContent === "O") {
+        turn = 'X'
+    click.textContent = `${turn}`;
+    }
+
+    else {
+        turn = "O"
+        click.textContent = `${turn}`;
+    }
+
+    
+}
+if (click.textContent !==""){
+        alert("the place is taken!!!!")
+    }
