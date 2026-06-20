f61553042e44e33b06a3f72f73e17388b86a8b5a
Nirjal Acharya <beasttale19@gmail.com>
2026-06-20 08:56:11 +0545

    I just wrote some codes to display what we enter. I think it might take very long time and lots of effort but i would make it anyhow ig.

--- games/js/calculator.js
+++ games/js/calculator.js
@@ -1,2 +1,13 @@
-let number = Number(document.getElementsByTagName("button"))
+let display = document.getElementById("screen")
+
+function press(value) {
+
+    if (display.textContent === "0") {
+        display.textContent = value;
+    }
+
+    else {
+        display.textContent += value;
+    }
+}
 
