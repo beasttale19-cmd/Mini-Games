473c91b12c772ed9b30999277b427b942bda4fd2
Nirjal Acharya <beasttale19@gmail.com>
2026-06-21 09:18:03 +0545

    Added all endings of win possible in tic tac toe, added alert to show when win game and will restart the page.

--- games/js/tic.js
+++ games/js/tic.js
@@ -10,6 +10,7 @@ let t7=document.getElementById("7");
 let t8=document.getElementById("8");
 let t9=document.getElementById("9");
 let textError=document.getElementById("error")
+let winTxt=document.getElementById("wonTxt")
 
 function press(value) {
     const click = document.getElementById(value);
@@ -33,16 +34,54 @@ function press(value) {
         click.textContent = `${turn}`;
     }
 
+    // ending1
     if (t1.textContent !== "" && t1.textContent === t2.textContent && t2.textContent === t3.textContent) {
-        alert(`${t1.textContent} Won the game.`)
+        alert(`${t1.textContent} Won the game. RESTARTING...`)
+        window.location.reload();
     }
 
+    // ending2
     if (t1.textContent !== "" && t1.textContent === t5.textContent && t5.textContent === t9.textContent) {
-        alert(`${t1.textContent} Won the game.`)
+        alert(`${t1.textContent} Won the game. RESTARTING...`)
+        window.location.reload();
     }
-    
-    if (t1.textContent !== "" && t1.textContent === t5.textContent && t5.textContent === t9.textContent) {
-        alert(`${t1.textContent} Won the game.`)
+
+    // ending3
+    if (t3.textContent !== "" && t3.textContent === t5.textContent && t5.textContent === t7.textContent) {
+        alert(`${t3.textContent} Won the game. RESTARTING...`)
+        window.location.reload();
     }
+
+    // ending4
+    if (t4.textContent !== "" && t4.textContent === t5.textContent && t5.textContent === t6.textContent) {
+        alert(`${t4.textContent} Won the game. RESTARTING...`)
+        window.location.reload();
+    }
+
+    // ending5
+    if (t7.textContent !== "" && t7.textContent === t8.textContent && t8.textContent === t9.textContent) {
+        alert(`${t7.textContent} Won the game. RESTARTING...`)
+        window.location.reload();
+    }
+
+    // ending6
+    if (t1.textContent !== "" && t1.textContent === t4.textContent && t4.textContent === t7.textContent) {
+        alert(`${t1.textContent} Won the game. RESTARTING...`)
+        window.location.reload();
+    }
+
+    // ending7
+    if (t3.textContent !== "" && t3.textContent === t6.textContent && t6.textContent === t9.textContent) {
+        alert(`${t3.textContent} Won the game. RESTARTING...`)
+        window.location.reload();
+    }
+
+    // ending8
+    if (t2.textContent !== "" && t2.textContent === t5.textContent && t5.textContent === t8.textContent) {
+        alert(`${t2.textContent} Won the game. RESTARTING...`)
+        window.location.reload();
+    }
+
+
   
 }
