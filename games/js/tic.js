353dd2de320b0826a650c384babf6b7577e7cd29
Nirjal Acharya <beasttale19@gmail.com>
2026-06-21 09:04:36 +0545

    added more js like which turn, who won the game (hasnt made all the endings but i have made somes and added more js like  Errors (if place is taken) and timeout is set to remove the error.

--- games/js/tic.js
+++ games/js/tic.js
@@ -9,11 +9,20 @@ let t6=document.getElementById("6");
 let t7=document.getElementById("7");
 let t8=document.getElementById("8");
 let t9=document.getElementById("9");
+let textError=document.getElementById("error")
 
 function press(value) {
     const click = document.getElementById(value);
-    click.textContent = `${turn}`;
+    if (click.textContent !==""){
+        textError.textContent = `ERRORS: THE PLACE IS TAKEN ALREADY!`
+    setTimeout(() =>{
+        textError.textContent = `ERRORS:`
+    },500)
+    return;
+    }
 
+    click.textContent = `${turn}`;  
+    
     if (click.textContent === "O") {
         turn = 'X'
     click.textContent = `${turn}`;
@@ -24,8 +33,16 @@ function press(value) {
         click.textContent = `${turn}`;
     }
 
-    
-}
-if (click.textContent !==""){
-        alert("the place is taken!!!!")
+    if (t1.textContent !== "" && t1.textContent === t2.textContent && t2.textContent === t3.textContent) {
+        alert(`${t1.textContent} Won the game.`)
     }
+
+    if (t1.textContent !== "" && t1.textContent === t5.textContent && t5.textContent === t9.textContent) {
+        alert(`${t1.textContent} Won the game.`)
+    }
+    
+    if (t1.textContent !== "" && t1.textContent === t5.textContent && t5.textContent === t9.textContent) {
+        alert(`${t1.textContent} Won the game.`)
+    }
+  
+}
