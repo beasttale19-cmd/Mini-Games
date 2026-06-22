68032fa0642a15357e43499e892bc95aa441a24b
Nirjal Acharya <beasttale19@gmail.com>
2026-06-22 21:53:11 +0545

    Oh, i forgot to write a commit, I completed the css of rock paper sciccor for now, now am working on js , i have let all the element some variables of id and give id to elements till now.....

---
+++ games/js/rps.js
@@ -0,0 +1,15 @@
+let botRock = document.getElementById("bot-rock")
+let botScissor = document.getElementById("bot-scissor")
+let botPaper = document.getElementById("bot-paper")
+
+let clickRock = document.getElementById("rock")
+let clickScissor = document.getElementById("scissor")
+let clickPaper = document.getElementById("paper")
+
+let choose = document.getElementById("choose")
+let botChoose = document.getElementById("botChoose")
+
+let random = Math.floor(Math.random() * 3);
+
+if (random = "1")
+
