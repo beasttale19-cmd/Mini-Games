91b1953ee27c7b8df5ee1ab466a292b6f9334108
Nirjal Acharya <beasttale19@gmail.com>
2026-06-23 21:36:16 +0545

    made the game very much better, It now works completely for Rock like i added if else statements for random like if random is 0 then the bot choose smth, if its 1 bot choose another, Now the js is left for paper and scissor.

--- games/js/rps.js
+++ games/js/rps.js
@@ -9,7 +9,68 @@ let clickPaper = document.getElementById("paper")
 let choose = document.getElementById("choose")
 let botChoose = document.getElementById("botChoose")
 
-let random = Math.floor(Math.random() * 3);
+let result = document.getElementById("result")
 
-if (random = "1")
+// alert(`${random} is random`)
+clickRock.onclick = function () {
 
+    let random = Math.floor(Math.random() * 3);
+
+    choose.style.display = "none";
+    botChoose.style.display = "flex";
+
+    if (random === 0) {
+        botRock.style.display = "flex";
+        result.textContent = `Result: Tied, try again :D`
+    }
+
+    else if (random === 1) {
+        botScissor.style.display = "flex";
+        result.textContent = `Result: You WON!`
+    }
+    else if (random === 2) {
+        botPaper.style.display = "flex";
+        result.textContent = `Result: You lose, Better luck next time! :D`
+    }
+}
+
+clickPaper.onclick = function () {
+
+    let random = Math.floor(Math.random() * 3);
+
+    choose.style.display = "none";
+    botChoose.style.display = "flex";
+
+    if (random === 0) {
+        botRock.style.display = "flex";
+        result.textContent = `Result: Tied, try again :D`
+    }
+
+    else if (random === 1) {
+        botScissor.style.display = "flex";
+        result.textContent = `Result: You Won!`
+    }
+    else if (random === 2) {
+        botPaper.style.display = "flex";
+        result.textContent = `Result: You Lose :), Beter Luck Next Time!`
+    }
+}
+
+clickScissor.onclick = function () {
+
+    let random = Math.floor(Math.random() * 3);
+
+    choose.style.display = "none";
+    botChoose.style.display = "flex";
+
+    if (random === 0) {
+        botRock.style.display = "flex";
+    }
+
+    else if (random === 1) {
+        botScissor.style.display = "flex";
+    }
+    else if (random === 2) {
+        botPaper.style.display = "flex";
+    }
+}
