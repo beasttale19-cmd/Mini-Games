90ee03aa5be05443caa78ef86cc9d20f33b482a9
Nirjal Acharya <beasttale19@gmail.com>
2026-06-27 19:59:00 +0545

    I finally made animations jump (not much) little, ig i am regretting to made ts game, it took me so much time with this js (syntax, code running anything like anything)

--- games/js/game.js
+++ games/js/game.js
@@ -2,15 +2,25 @@ const player = document.getElementById("player")
 
 const idle = document.getElementById("idle")
 const right = document.getElementById("right")
+const left = document.getElementById("left")
+const jump = document.getElementById("jump")
 
 let playerX = 0;
+let playerY = 0;
 
 window.addEventListener('keydown', move);
+window.addEventListener('keyup', dontMove);
 
-function move(event){
-    if (event.key === 'ArrowRight'){
-        playerX = playerX + 10;
-        player.style.transform = `translateX(${playerX}px)`;
+let leftKey = "ArrowLeft"
+let rightKey = "ArrowRight"
+let aKey = "a"
+let dKey = "d"
+aKey = "leftKey"
+rightKey = ""
+
+function move(event) {
+    if (event.key === 'ArrowRight' || event.key === 'd') {
+        playerX = playerX + 5;
 
         idle.classList.add("none");
         right.classList.remove("none");
@@ -18,8 +28,53 @@ function move(event){
         right.style.display = "block";
     }
 
-    if (event.key === 'ArrowLeft'){
-        playerX = playerX - 10;
-        player.style.transform = `translateX(${playerX}px)`;
+    if (event.key === ' ' || event.key === 'ArrowUp' || event.key === 'w') {
+        playerY = playerY - 50;
+        idle.classList.add("none");
+        jump.classList.remove("none");
     }
+
+    if (event.key === 'ArrowLeft' || event.key === 'a') {
+        playerX = playerX - 5;
+
+        idle.classList.add("none");
+        left.classList.remove("none");
+        idle.style.display = "none";
+        left.style.display = "block";
+    }
+
+    // if (event.key === 'ArrowLeft' || event.key === 'a' && event.key === 'ArrowRight' || event.key === 'd') {
+    //     left.classList.add("none");
+    //     idle.classList.remove("none");
+    //     left.style.display = "none";
+    //     idle.style.display = "block";
+    //     right.classList.add("none")
+    //     // right.style.display("none")
+    // }
+
+    player.style.transform = `translateX(${playerX}px) translateY(${playerY}px)`;
+}
+
+function dontMove(event) {
+    if (event.key === 'ArrowRight' || event.key === 'd') {
+        right.classList.add("none");
+        idle.classList.remove("none");
+        right.style.display = "none";
+        idle.style.display = "block";
+    }
+
+    if (event.key === 'ArrowLeft' || event.key === 'a') {
+        left.classList.add("none");
+        idle.classList.remove("none");
+        left.style.display = "none";
+        idle.style.display = "block";
+    }
+
+     if (event.key === ' ' || event.key === 'w') {
+        playerY = playerY + 50;
+        jump.classList.add("none");
+        idle.classList.remove("none");
+    }
+
+    player.style.transform = `translateX(${playerX}px) translateY(${playerY}px)`;
 }
